1e072a66d3fb041a59c964009eb5ba96518a4ab6
key2-hue <keymaeda3782@gmail.com>
2019-07-20 13:45:45 +0900

    changed

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,32 @@
 $(function() {
+  function buildHTML(message) {
+    var content =  message.content ? `<p class="lower-message__content">${message.content}</P>` : "";
+    var image = message.image ? `<img class="lower-message__image" src="${message.image}">` : "";
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                    ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                    ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    ${content}
+                    ${image}
+                  </div>
+                </div>`
+    return html;
+  }
+
+  function lastMessage(){
+    var last = $('.message').last();
+    var position = last.offset().top + $('.messages').scrollTop();
+    $('.messages').animate({
+      scrollTop: position
+    },400);
+  }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +39,30 @@ $(function() {
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.submit__type').val('')
+      console.log(data); 
+      lastMessage();     
+    })
+    .fail(function(data) {
+      alert('メッセージを送信できませんでした');
+    })
+    .always(function(data) {
+      $('.submit__button').attr('disabled',false);
+    })
   })
+
+  $('.submit__button').on('dblclick', function(e) {
+    e.preventDefault();
+    alert('連続では押せません');
+    $.ajax ({
+      
+    })
+    .always(function(data) {
+      $('.submit__button').attr('disabled',false);
+    })
+  });
+
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,2 +1,5 @@
-json.comment @message.comment
-json.image @message.image
+json.id @message.id
+json.content @message.content
+json.image @message.image.url
+json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.user_name @message.user.name
