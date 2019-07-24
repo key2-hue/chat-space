d00444cff3752d8fc4442c0c8cddb710b1b8b99c
key2-hue <keymaeda3782@gmail.com>
2019-07-24 14:46:01 +0900

    capistrano fix5

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# set :deploy_to, "/var/www/chat-space"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
